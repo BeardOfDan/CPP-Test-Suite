20e41a10776ed2ae8193bf6a07ab724d41931a67
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-31 00:46:00 -0700

    (feat) Add method "getVectorOfLines" to struct redirectOutput
    Found in coutRedirect.h
    
    It returns a vector<string> of the content, with a default char
    delimiter of '\n'.

--- coutRedirect.h
+++ coutRedirect.h
@@ -1,6 +1,10 @@
 // Note: The below struct was inspired by the answer by "Björn Pollex"
 //   https://stackoverflow.com/questions/5419356/redirect-stdout-stderr-to-a-string
 
+// Note: The below function "getVectorOfLines" uses a method, to break up
+// strings by a delimiter, that was from the answer by "Evan Teran"
+//   https://stackoverflow.com/questions/236129/the-most-elegant-way-to-iterate-the-words-of-a-string
+
 #include <iostream>
 using std::cout;
 
@@ -13,6 +17,9 @@ using std::stringstream;
 #include <string>
 using std::string;
 
+#include <vector>
+using std::vector;
+
 struct redirectOutput {
   redirectOutput() : originalCoutBuffer(cout.rdbuf()) {
     cout.rdbuf(capturedOutput.rdbuf());  // capture cout's output
@@ -23,6 +30,19 @@ struct redirectOutput {
   }
 
   string getContents() { return capturedOutput.str(); }
+
+  vector<string> getVectorOfLines(char delimiter = '\n') {
+    const string str = capturedOutput.str();
+    vector<string> result;
+
+    stringstream ss(str);
+    string line;
+    while (getline(ss, line, delimiter)) {
+      *(back_inserter(result)++) = line;
+    }
+
+    return result;
+  }
 
  private:
   streambuf* originalCoutBuffer;

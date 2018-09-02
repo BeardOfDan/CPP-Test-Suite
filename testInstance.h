07dda9c0904e46871106d8f462ba5b6d27d10372
Daniel Roberts <BeardOfDan@gmail.com>
2018-09-01 22:01:33 -0700

    (documentation) Update testInstance.h comments

--- testInstance.h
+++ testInstance.h
@@ -13,6 +13,22 @@ using std::tuple;
 // and implement here, or extend this class to create a testInstance that allows
 // for a particular test function to be used (since not all functions to test
 // will automatically pipe their result to cout)
+// ---- Possible implementation: create a generic function that simply executes
+// functionToTest(arguments) in the same way it currently is. This function will
+// be the default value for an additional constructor argument that allows a
+// tester to define particular implementations of their tests.
+
+template <typename Func, typename tup>
+string defaultFunc(Func f, tup t) {
+  redirectOutput output;
+
+  apply(f, t);
+
+  return output.getContents();
+}
+
+// possible constructor arg format
+// , Func defFunc = defaultFunc
 
 class testInstance {
  public:
@@ -28,7 +44,7 @@ class testInstance {
     apply(functionToTest, arguments);
     actualOutput = output.getContents();
   }  // Note: the destructor for redirectOutput is called here
-     // (affects cout's flow)
+     // (restores cout's usual flow)
 
   ~testInstance() {}
 

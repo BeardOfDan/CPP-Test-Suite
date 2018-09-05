7e998e26d270ae87889d0fdae629152eab6de840
Daniel Roberts <BeardOfDan@gmail.com>
2018-09-05 00:43:48 -0700

    (feat) Create toString function for any type to string conversion
    Since it has to be done at many different points in the class, I
    wanted a short (code length wise) implementation, and it turns
    out that to_string will throw an error if you give it a string...

--- generalLibrary.h
+++ generalLibrary.h
@@ -1,7 +1,13 @@
 
+#ifndef GENERAL_LIBRARY_H
+#define GENERAL_LIBRARY_H
+
 #include <string>
 using std::string;
 using std::to_string;
+
+#include <sstream>
+using std::ostringstream;
 
 template <typename type>
 string arrToString(type* arr, int size, string delimiter = ", ") {
@@ -11,3 +17,15 @@ string arrToString(type* arr, int size, string delimiter = ", ") {
   }
   return (s + "]");
 }
+
+// This function is required since the templated input types have to convert any
+// given type to a string, but the different types require different conversion
+// methods (assuming single line implementation)
+template <typename type>
+string toString(type value) {
+  ostringstream oss;
+  oss << value;
+  return oss.str();
+}
+
+#endif
